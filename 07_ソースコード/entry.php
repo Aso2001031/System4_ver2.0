a98e30baf142aa70df427603352ecbb9303a6365
Aso2001007 <83050889+Aso2001007@users.noreply.github.com>
2022-06-02 12:27:49 +0900

    Add files via upload

---
+++ 07_ソースコード/entry.php
@@ -0,0 +1,39 @@
+<?php session_start();?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h1>会員登録</h1>
+<?php
+$name=$pass=$mail='';
+if (isset($_SESSION['member'])) {
+    $name=$_SESSION['member']['name'];
+    $pass=$_SESSION['member']['pass'];
+    $mail=$_SESSION['member']['mail'];
+    
+}
+
+echo '<form action="check.php" method="post" enctype=""multipart/from-data>';
+
+echo '<p>お名前</p>';
+echo '<input type="text" name="name" placeholder="名前を入力してください" value="',$name,'">','<br>';
+
+echo '<p>メールアドレス</p>';
+echo '<input type="text" name="mail" placeholder="メールアドレスを入力してください" value="',$mail,'">','<br>';
+
+echo '<p>パスワード</p>';
+echo '<input type="password" name="pass" placeholder="英大文字,小文字,数字が1文字以上含まれてる8文字以上24文字以下" value="',$pass,'">','<br>';
+/*
+echo '<p>ユーザーアイコン(任意)</p>';
+echo '<input type="file" name="img">';
+*/
+echo '<input type="submit" class="register-submit" value="確定">';
+echo '</form>';
+echo '</div>';
+?>
+</body>
+</html>
