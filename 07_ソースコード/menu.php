a76f9ced3d295c46ccfb381418586af5c2fd8d06
Aso2001007 <83050889+Aso2001007@users.noreply.github.com>
2022-07-08 11:00:55 +0900

    Add files via upload

--- 07_ソースコード/menu.php
+++ 07_ソースコード/menu.php
@@ -9,5 +9,6 @@
         <a href="http://aso2001007.versus.jp/System4_Ver2.0/entry.php">会員登録画面へ</a>
         <a href="http://aso2001007.versus.jp/System4_Ver2.0/edit.php">編集画面へ</a>
         <a href="http://aso2001007.versus.jp/System4_Ver2.0/user_post.php">投稿一覧へ</a>
+        <a href="http://aso2001007.versus.jp/System4_Ver2.0/group_post.php">グループ投稿一覧へ</a>
     </body>
 </html>
